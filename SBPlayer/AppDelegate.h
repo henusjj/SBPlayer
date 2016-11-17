f19eff2c80421c869cce9b2f19c002f0844849ac
WalkWithWind <sycf_ios_13579@sycf-iosdeiMac.local>
2016-11-17 10:54:29 +0800

    Initial Commit

---
+++ SBPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SBPlayer
+//
+//  Created by sycf_ios on 2016/11/17.
+//  Copyright © 2016年 sycf_ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SBPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SBPlayer
+//
+//  Created by sycf_ios on 2016/11/17.
+//  Copyright © 2016年 sycf_ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
